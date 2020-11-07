f0a39920748caf78081dc200a952e404a862bc79
Tayyaba <tayyaba.asad1234@gmail.com>
2020-11-07 09:23:43 -0500

    added stuff to html and script file

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ $(document).ready(function (){
         searchWeather(searchValue)
     })
 
+    function makeRow(text) {
+        var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+        $(".history").append(li);
+      }
+
     function searchWeather(searchValue) {
         $.ajax({
           type: "GET",
@@ -27,11 +32,29 @@ $(document).ready(function (){
 
         var title= $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
         var cardBody= $("<div>").addClass("card-body") 
-            cardBody.append(title)
+        var card = $("<div>").addClass("card");
+        var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+        var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+        var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
+        var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");    
+        
+        
+        
+        title.append(img);
+        cardBody.append(title, temp, humid, wind);
+        card.append(cardBody);
+        $("#today").append(card);
+        // getForecast(searchValue);
+        // getUVIndex(data.coord.lat, data.coord.lon);
             
     
     }
 
     })
     }
+
+
+
+
+
 })
